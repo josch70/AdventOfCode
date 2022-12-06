4f76407e38c567016949bd2f50dd2da885d1d124
Joachim Dill <j.dill@telekom.de>
2022-12-06 07:07:12 +0100

    2022 Day06

---
+++ 2022/06/answer1.js
@@ -0,0 +1,29 @@
+const path = require("path");
+var fs = require("fs");
+
+const inputarray = fs  
+    .readFileSync(path.resolve(__dirname, "input.txt"), "utf-8")  
+    .split(/\r?\n/)
+    // .map(Number);
+
+
+function hasDuplicates(array) {
+    return (new Set(array)).size !== array.length;
+}
+
+let count = 4; //Number of processed characters
+
+
+for (let i=0;i<inputarray[0].length;i++){
+    const four = inputarray[0].slice(i,4+i); //returns array of 4 characters from input in a loop
+    let dupl = hasDuplicates(four); //checks if array of 4 characters hast duplicates
+    if (!dupl) {
+        console.log(`${count} characters needs to be processed.`); //if array of 4 characters has no duplicates break loop and print result
+        break;
+    }
+    else{
+        count += 1; //if array of 4 characters has still duplicates increase counters
+    };
+    
+}
+
